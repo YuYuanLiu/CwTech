4121b58dc16316c593cdcc1807b8f0c6cb8dd8db
tsaiyuchi <tsaiyuchi88@gmail.com>
2018-01-19 15:52:37 +0800

    Update FileStorage.cs

--- SensingNet/Storage/FileStorage.cs
+++ SensingNet/Storage/FileStorage.cs
@@ -55,7 +55,7 @@ namespace SensingNet.Storage
                 var dir = System.IO.Path.Combine(signalCfg.StorageDirectory, "dt" + now.ToString("yyyyMMdd"));
 
                 cd.CreateStreamIfNewFile(dir, fn);
-                var timestamp = CToolkit.DateTimeStamp.ToTimeStamp(now);
+                var timestamp = CToolkit.DateTimeStamp.ToUniversalTimestamp(now);
                 cd.stream.Write("{0}", timestamp);
                 for (int idx = 0; idx < ea.Data.Count; idx++)
                     cd.stream.Write(",{0}", ea.calibrateData[idx]);
@@ -90,7 +90,7 @@ namespace SensingNet.Storage
                     DeleteOldCurrent(dir);
 
                 }
-                var timestamp = CToolkit.DateTimeStamp.ToTimeStamp(now);
+                var timestamp = CToolkit.DateTimeStamp.ToUniversalTimestamp(now);
                 cd.stream.Write("{0}", timestamp);
                 for (int idx = 0; idx < ea.Data.Count; idx++)
                     cd.stream.Write(",{0}", ea.calibrateData[idx]);
