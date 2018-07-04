8959f2c1a59f168386b8cb914e90176df062b231
tsaiyuchi <tsaiyuchi88@gmail.com>
2018-07-05 00:42:06 +0800

    auto commit

--- SensingNet.MyTest/UtSampleCaptureSignal.cs
+++ SensingNet.MyTest/UtSampleCaptureSignal.cs
@@ -15,48 +15,30 @@ namespace SensingNet.MyTest
         [TestMethod]
         public void TestMethod()
         {
-            var length = 2048;
-            var samplingRate = 512;
-            var baseWave = Generate.Sinusoidal(length, samplingRate, 1, 24);
-            var abnormalWave = Generate.Sinusoidal(length, samplingRate, 16, 5);
 
-            var finalWave = new double[baseWave.Length];
-            for (int idx = 0; idx < finalWave.Length; idx++)
+
+            var deviceCfg = new Signal.DeviceCfg();
+            deviceCfg.RemoteIp = "192.168.123.201";
+            deviceCfg.RemotePort = 10002;
+
+            var signalCfg = new Signal.SignalCfg();
+            deviceCfg.SignalCfgList.Add(signalCfg);
+            signalCfg.DeviceSvid = 0x00010000;
+
+
+            var signalHandler = new Signal.SignalHandler();
+            signalHandler.config = deviceCfg;
+
+            try
             {
-                var a = baseWave[idx];
-                var waveLoopIdx = idx % samplingRate;
-                var b = (waveLoopIdx > samplingRate / 2 - 32) && (waveLoopIdx < samplingRate / 2 + 32) ? abnormalWave[idx] : 0;
-                abnormalWave[idx] = b;
-                finalWave[idx] = a + b;
+                signalHandler.CfInit();
+                signalHandler.CfLoad();
+                while (true) { signalHandler.CfExec(); }
             }
-
-
-
-
-            var npContext = new CToolkit.NumericProc.NpContext();
-            var baseFft = npContext.FftForward(baseWave);
-            var abnormalFft = npContext.FftForward(abnormalWave);
-            var finalFft = npContext.FftForward(finalWave);
-
-
-            using (var f = File.Open("data.csv", FileMode.Create))
-            using (var sw = new StreamWriter(f))
+            finally
             {
-                sw.WriteLine("Wave,a,b,a+b");
-                for (int idx = 0; idx < baseWave.Length; idx++)
-                {
-
-
-                    sw.WriteLine("{0},{1},{2},{3},{4},{5},{6}",
-                        idx,
-                        baseWave[idx],
-                        abnormalWave[idx],
-                        finalWave[idx],
-                        baseFft[idx].Magnitude,
-                        abnormalFft[idx].Magnitude,
-                        finalFft[idx].Magnitude
-                        );
-                }
+                signalHandler.CfUnload();
+                signalHandler.CfFree();
             }
 
 
