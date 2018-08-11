dc2ce66d182ee34463f628853d8a19b3e77b767a
tsaiyuchi <tsaiyuchi88@gmail.com>
2018-08-11 14:22:11 +0800

    bat commit

--- SensingNet/LoggerAssembly.cs
+++ SensingNet/LoggerAssembly.cs
@@ -9,17 +9,24 @@ namespace SensingNet
     public class LoggerAssembly
     {
         public static string LoggerAssemblyName { get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Name; } }
-        public static Logger Logger { get { return LoggerMapper.Singleton[LoggerAssemblyName]; } }
-
+        public static Logger Logger { get { return LoggerMapper.Singleton.Get(LoggerAssemblyName); } }
 
         public static void Write(LoggerEventArgs ea)
         {
             Logger.Write(ea);
         }
-
-        public static void Write(String format, params object[] args)
+        public virtual void Write(LoggerEventArgs ea, LoggerEnumLevel _level)
         {
-            Logger.Write(string.Format(format, args));
+            ea.level = _level;
+            Logger.Write(ea);
         }
+
+
+        public virtual void Verbose(string msg, params object[] args) { Logger.Write(string.Format(msg, args), LoggerEnumLevel.Verbose); }
+        public virtual void Debug(string msg, params object[] args) { Logger.Write(string.Format(msg, args), LoggerEnumLevel.Debug); }
+        public virtual void Info(string msg, params object[] args) { Logger.Write(string.Format(msg, args), LoggerEnumLevel.Info); }
+        public virtual void Warn(string msg, params object[] args) { Logger.Write(string.Format(msg, args), LoggerEnumLevel.Warn); }
+        public virtual void Error(string msg, params object[] args) { Logger.Write(string.Format(msg, args), LoggerEnumLevel.Error); }
+        public virtual void Fatal(string msg, params object[] args) { Logger.Write(string.Format(msg, args), LoggerEnumLevel.Fatal); }
     }
 }
